cea94742474ebb914df72ca707481d8dba4c7c73
kesihinz <keishin.chou@gmail.com>
2019-10-26 09:10:37 +0900

    Initial Commit

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Keishin CHOU on 2019/10/26.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
